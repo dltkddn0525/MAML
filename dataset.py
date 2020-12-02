6090a589b7d8f7cd30b63ab771369efe54c7e22e
dltkddn0525 <dltkddn0525@naver.com>
2020-12-02 17:19:11 +0900

    new file

---
+++ dataset.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import os
+import json
+import time
+from gensim.models.doc2vec import Doc2Vec
+from torch.utils.data import Dataset
+
+def load_data(path):
+    train_df = pd.read_csv(os.path.join(path,'train.csv'), index_col=None, usecols=None)
+    test_df = pd.read_csv(os.path.join(path,'test.csv'), index_col=None, usecols=None)
+
+    # item ID = 2405 split: train 3 test 2
+    item_2405 = test_df[test_df["itemID"]==2405][:3]
+    train_df = train_df.append(item_2405)
+    test_df = test_df.drop(item_2405.index)
+
+    # Inspect : at least 3 interaction per user for train, 2 per user for test
+    #train_x_user, train_x_num_rating = inspect(train_df,3)
+    #test_x_user, test_x_num_rating = inspect(test_df,2)
+    #print(f"Inspectation : Train = {len(train_x_user)} Users, Test = {len(test_x_user)} Users")
+
+    num_user = max(train_df["userID"])+1
+    num_item = max(train_df["itemID"])+1
+
+    end = time.time()
+    test_negative = []
+    train_ng_pool = []
+
+    total_item = np.arange(0,num_item)
+
+    for user in range(num_user):
+        # train ng pool = Every item - user's positive item
+        positive_item = train_df[train_df['userID']==user]['itemID'].tolist()
+        train_ng_item_u = np.setdiff1d(total_item,positive_item).tolist()
+        test_ng_item_u = np.setdiff1d(total_item,positive_item).tolist()
+        # Test negative = Every item - user's positive item
+        test_negative.append(test_ng_item_u)
+        train_ng_pool.append(train_ng_item_u)
+
+    # train_dataset = train_df.values.tolist()
+    test_df = pd.DataFrame(test_df[['userID','itemID']])
+    test_df['rating'] = 1
+
+    doc2vec_model = Doc2Vec.load(os.path.join(path, 'doc2vecFile'))
+    vis_vec = np.load(os.path.join(path, 'image_feature.npy'),allow_pickle=True).item()
+
+    asin_dict = json.load(open(os.path.join(path, 'asin_sample.json'), 'r'))
+
+    text_vec = {}
+    for asin in asin_dict:
+        text_vec[asin] = doc2vec_model.docvecs[asin]
+
+    asin_i_dic = {}
+    for index, row in train_df.iterrows():
+        asin, i = row['asin'], int(row['itemID'])
+        asin_i_dic[i] = asin
+
+    t_features = []
+    v_features = []
+    for i in range(num_item):
+        t_features.append(text_vec[asin_i_dic[i]])
+        v_features.append(vis_vec[asin_i_dic[i]])
+
+    return train_df, test_df, test_negative, train_ng_pool, num_user, num_item, np.array(t_features), np.array(v_features)
+
+def concat_train_dataset(df, negative, text_feature, visual_feature):
+    end = time.time()
+    negative = np.array(negative)
+    temp_df = pd.DataFrame(df[["userID","itemID"]])
+    temp_df["negative"] = negative[temp_df["userID"]]
+    ng_dataset=[]
+    for index, row in temp_df.iterrows():
+        user, item_p, ng_pool = int(row["userID"]), int(row["itemID"]), row["negative"]
+        item_n = []
+        for i in range(4):
+            idx = np.random.randint(0,len(ng_pool))
+            item_n.append(ng_pool[idx])
+        ng_dataset.append(item_n)
+            
+    feature = np.concatenate((text_feature, visual_feature),axis=1)
+    del temp_df["negative"]
+    print(f"Time : {time.time()-end:.4f}")
+    return temp_df.values.tolist(), ng_dataset, feature
+
+
+class CustomDataset(Dataset):
+    def __init__(self, dataset, feature, negative, istrain=False):
+        super(CustomDataset,self).__init__()
+        self.dataset = dataset
+        self.feature = feature
+        self.negative = negative
+        self.istrain = istrain
+
+        if not istrain:
+            self.dataset = self.make_testset()
+
+    def make_testset(self):
+        ng_samples = []
+        users = np.unique(self.dataset["userID"])
+        for user in users:
+            ng_pool = self.negative[user]
+            for item in ng_pool:
+                ng_samples.append([user,item,0])
+
+        dataset = self.dataset.values.tolist() + ng_samples
+        return dataset
+        
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        if self.istrain:
+            user, item_p = self.dataset[index]
+            item_n = self.negative[index]
+            feature_p = self.feature[item_p]
+            feature_n = self.feature[item_n]
+            return user, item_p, item_n, feature_p, feature_n
+        else:
+            user, item, label = self.dataset[index]
+            feature = self.feature[item]
+            return user, item, feature, label
+
+
+def inspect(df,num_inter):
+    user = np.unique(df["userID"])
+    x_user = []
+    x_num_rating = []
+    for i in user:
+        if len(df[df["userID"]==i])<num_inter:
+            x_user.append(i)
+            x_num_rating.append(len(df[df["userID"]==i]))
+
+    return x_user, x_num_rating
--- main.py
+++ main.py
@@ -3,14 +3,31 @@ import argparse
 import json
 import time
 import os
+from torch.utils.data import DataLoader
 
 from utils import Logger, AverageMeter
 from model import MAML
 from loss import Embedding_loss, Feature_loss
+import dataset as D
+
 
 parser = argparse.ArgumentParser(description='MAML')
 parser.add_argument('--save_path', default='./result', type=str,
                      help='savepath')
+parser.add_argument('--batch_size', default=128, type=int,
+                     help='batch size')
+parser.add_argument('--epoch', default=100, type=int,
+                     help='train epoch')
+parser.add_argument('--data_path', default='./Data/Office', type=str,
+                     help='Path to dataset')
+parser.add_argument('--embed_dim', default=64, type=int,
+                     help='Embedding Dimension')
+parser.add_argument('--dropout_rate', default=0.2, type=float,
+                     help='Dropout rate')
+parser.add_argument('--lr', default=0.001, type=float,
+                     help='Learning rate')
+parser.add_argument('--margin', default=1.6, type=float,
+                     help='Margin for embedding loss')
 
 args = parser.parse_args()
 
@@ -27,45 +44,100 @@ def main():
 
     # Load dataset
     '''
-    userID = [N]
-    positive itemID = [N]
-    positive item feature = [N,4096+512]
-    negative itemID = [N,num_neg]
-    negative item feature = [N,num_neg,4096+512]
+    Train Batch [user, item_p, item_n, feature_p, feature_n]
+    user = [N]
+    item_p = [N]
+    item_n = [N x num_neg]
+    feature_p = [N x (vis_feature_dim + text_feature_dim)]
+    featuer_n = [N x num_neg x (vis_feature_dim + text_feature_dim)]
+
+    Test Batch [user, item, feature]
+    user = [N]
+    item = [N]
+    feature = [N x (vis_feature_dim + text_feature_dim)]
     '''
+    path = args.data_path
+    train_dataset, test_dataset, test_negative, train_ng_pool, num_user, num_item, t_features, v_features = D.load_data(path)
+    train_dataset, train_negative, feature = D.concat_train_dataset(train_dataset, train_ng_pool, t_features, v_features)
+    train_dataset = D.CustomDataset(train_dataset, feature, negative=train_negative, istrain=True)
+    test_dataset = D.CustomDataset(test_dataset, feature, negative=test_negative, istrain=False)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, collate_fn=my_collate)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+
 
     # Model
-    n_users = 100
-    n_items = 120
-    embed_dim = 64
-    dropout_rate = 0.2
-    model = MAML(n_users, n_items, embed_dim, dropout_rate)
-    
-    ### Sample input
-    batch_size = 2
-    num_neg = 4
-    sample_user = torch.randint(0,10,(batch_size,))
-    sample_item_p = torch.randint(0,10,(batch_size,))
-    sample_item_feature_p = torch.randn(batch_size,4608)
-    sample_item_n = torch.randint(0,10,(batch_size,num_neg))
-    sample_item_feature_n = torch.randn(batch_size,num_neg,4608)
-    p_u, q_i, q_i_feature, dist_p = model(sample_user, sample_item_p,sample_item_feature_p)
-    p_u, q_k, q_k_feature, dist_n = model(sample_user, sample_item_n,sample_item_feature_n)
-    #####
-    
+    model = MAML(num_user, num_item, args.embed_dim, args.dropout_rate).cuda()
+        
     # Optimizer
-    optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(),lr=args.lr)
     
     # Loss 
-    embedding_loss = Embedding_loss(0.2)
-    feature_loss = Feature_loss()
+    embedding_loss = Embedding_loss(margin=args.margin).cuda()
+    feature_loss = Feature_loss().cuda()
+    # + Covariance Loss
 
     # Logger
     train_logger = Logger(f'{save_path}/train.log')
     test_logger = Logger(f'{save_path}/test.log')
 
     # Train & Eval
+    for epoch in range(args.epoch):
+        train(model, embedding_loss, feature_loss, optimizer, train_loader, train_logger, epoch)
 
+def train(model, embedding_loss, feature_loss, optimizer, train_loader, train_logger, epoch):
+    model.train()
+    total_loss = AverageMeter()
+    embed_loss = AverageMeter()
+    feat_loss = AverageMeter()
+    data_time = AverageMeter()
+    iter_time = AverageMeter()
+
+    end = time.time()
+    for i, (user, item_p, item_n, feature_p, feature_n) in enumerate(train_loader):
+        data_time.update(time.time()-end)
+        user, item_p, item_n, feature_p, feature_n = user.cuda(), item_p.cuda(), item_n.cuda(), feature_p.cuda(), feature_n.cuda()
+
+        p_u, q_i, q_i_feature, dist_p = model(user, item_p, feature_p)
+        _, q_k, q_k_feature, dist_n = model(user, item_n, feature_n)
+        # Loss
+        loss_e = embedding_loss(dist_p, dist_n)
+        loss_f = feature_loss(q_i, q_i_feature, q_k, q_k_feature)
+        loss = loss_e + 7 * loss_f
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss.update(loss.item())
+        embed_loss.update(loss_e.item())
+        feat_loss.update(loss_f.item())
+        iter_time.update(time.time()-end)
+        end = time.time()
+
+        if i % 10 == 0:
+            print(f"[{epoch+1}/{args.epoch}][{i}/{len(train_loader)}] Total loss : {total_loss.avg:.4f} \
+                Embedding loss : {embed_loss.avg:.4f} Feature loss : {feat_loss.avg:.4f} \
+                Iter time : {iter_time.avg:.4f} Data time : {data_time.avg:.4f}")
+
+    train_logger.write([epoch, total_loss.avg, embed_loss.avg,
+                        feat_loss.avg])
+
+def test(model, test_loader, test_logger, epoch):
+    model.eval()
+    
+
+def my_collate(batch):
+    user = [item[0]for item in batch]
+    user = torch.LongTensor(user)
+    item_p = [item[1] for item in batch]
+    item_p = torch.LongTensor(item_p)
+    item_n = [item[2] for item in batch]
+    item_n = torch.LongTensor(item_n)
+    feature_p = [item[3] for item in batch]
+    feature_p = torch.Tensor(feature_p)
+    feature_n = [item[4] for item in batch]
+    feature_n = torch.Tensor(feature_n)
+    return [user, item_p, item_n, feature_p, feature_n]
 
 if __name__ == "__main__":
     main()
